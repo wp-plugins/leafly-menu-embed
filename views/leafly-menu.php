2434ee87b5dc835c15f1e7fa340d6fcadc6d85e8
ted <tedgeving@users.noreply.github.com>
2016-09-12 16:53:58 -0600

    Updated Embed Code
    Replaced Outdated embed code  with current versions.

--- views/leafly-menu.php
+++ views/leafly-menu.php
@@ -1,1 +1,4 @@
-<div id="leafly-menu"><iframe src="http://www.leafly.com/embed/menu/<?php echo $content ?>?initialWidth=1029&amp;childId=leafly-menu" width="100%" scrolling="no" marginheight="0" frameborder="0" height="6347px"></iframe></div>
+<div id="leafly-menu"></div>
+<div style="text-align:center;">Visit our profile on <a href="<?php echo $content ?>">Leafly</a></div>
+<script src="https://www.leafly.com/public/global/js/dispensarymanager/embed.js"></script>
+<script>var pymParent = pym.Parent('leafly-menu', 'https://www.leafly.com/embed/menu2/ultra-health-albuquerque---ne', {});</script>
