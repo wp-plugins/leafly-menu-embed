8cba146d9905021ee2de7985f1b7cbb8ac3d8165
ted <tedgeving@users.noreply.github.com>
2016-09-13 10:36:41 -0600

    Update leafly-menu.php

--- views/leafly-menu.php
+++ views/leafly-menu.php
@@ -1,4 +1,3 @@
 <div id="leafly-menu"></div>
-<div style="text-align:center;">Visit our profile on <a href="<?php echo $content ?>">Leafly</a></div>
 <script src="https://www.leafly.com/public/global/js/dispensarymanager/embed.js"></script>
 <script>var pymParent = pym.Parent('leafly-menu', 'https://www.leafly.com/embed/menu2/ultra-health-albuquerque---ne', {});</script>
